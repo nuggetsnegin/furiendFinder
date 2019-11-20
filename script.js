b0cc8696c6aa59b1d052f7b09116d7e032e3be37
Negin <nuggetnegin@gmail.com>
2019-11-20 18:25:39 -0500

    grabbing age and total cats in age group from petfinder Api and appending

---
+++ script.js
@@ -0,0 +1,67 @@
+/*create empty object of app*/
+const furiendFinder = {};
+
+/*storing petFinder api key*/
+furiendFinder.petFinderApiKey = "bMlZfEidmf7kNocIPx9z0lC62XwFTmFu94bYQFRRzc8sxD9x6I";
+
+/*needed for oauth*/
+furiendFinder.secretPetFinder = "Lwa5bb5fyzQ51KMtEbbJb8QWgItW49OsEuYqgElG";
+
+/*init method, on page load*/
+furiendFinder.init = function(){
+
+    const petAgeArray = ["baby", "young", "adult", "senior"];
+    /*run a for loop 4 times (0-3) and using the getPetsAvailable method to grab the pet's age and using the petFinder api query to get those words into the function*/
+    for(let i = 0; i < 4; i++){
+        furiendFinder.getPetsAvailable(petAgeArray[i]);
+    }
+
+}
+
+/*find all the available pets for adoption*/
+furiendFinder.getPetsAvailable = function(petAge){
+
+    $.ajax({
+        url: "https://api.petfinder.com/v2/oauth2/token",
+        method: "POST", /*needed for ouath*/
+        data: {
+            grant_type: "client_credentials",
+            client_id: furiendFinder.petFinderApiKey,
+            client_secret: furiendFinder.secretPetFinder,
+        }
+        }).then((token)=>{
+            $.ajax({
+                url: "https://api.petfinder.com/v2/animals", /*base call*/
+            headers:{ /*for ouath*/
+                Authorization: `Bearer ${token["access_token"]}`,
+            },
+            data:{
+                age: petAge,
+                type: "cat",
+                status: "adoptable",
+                location: "Toronto, ON" /*change later to a variable*/
+            },
+            }).then((data)=>{
+                const totalPets = data.pagination["total_count"];
+                console.log(petAge);
+                console.log(totalPets);
+                furiendFinder.displayResults(totalPets, petAge);
+            })
+            
+        });
+
+}
+
+/*method to display results into the html*/
+furiendFinder.displayResults = function(totalPets, petAge){
+    $('.age').append(`<li><p>${petAge}</p><img src="http://placekitten.com/g/200/300"><p>${totalPets}</p></li>`);
+
+}
+
+// furiendFinder.getPetsAvailable();
+
+
+$(document).ready(function(){
+    furiendFinder.init();
+})
+
