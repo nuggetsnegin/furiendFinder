db7103e9257ea80bd6490ba1c505104b203f22a2
Juan Jose Acaiturri-Villa <juan.acaiturri@hotmail.com>
2019-11-21 20:58:03 -0500

    Added geolocation and user input blur event for the city

--- script.js
+++ script.js
@@ -10,28 +10,98 @@ furiendFinder.secretPetFinder = "p6b3lhtd4bGo4OxjTEJK3D0eYqllfDGRI6VIXUAF";
 /*storing cat api key*/
 furiendFinder.catApiKey = "796188a0-0bca-4abe-968b-403b12c2c82d";
 
+// MapQuest api Key
+furiendFinder.mapQuestKey = "gVcJABFXiDTymAro7AI80OiMBbUOCygN";
+
 // storing pet type
 furiendFinder.petType = "cat";
+
+// Making a property to store the user's city
+furiendFinder.city = "";
+
+// Geolocation function to get the city and province of the user. If the geolocation is not allowed then Toronto will be the default.
+
+furiendFinder.getGeoLocation = () => {
+
+    const success = (position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        
+        $.ajax({
+            url: `http://open.mapquestapi.com/geocoding/v1/reverse/`,
+            data: {
+                key: furiendFinder.mapQuestKey,
+                location: `${latitude},${longitude}`
+            }
+        }).then((data) => {
+            console.log(data);
+            const city = data.results[0].locations[0]["adminArea5"];
+            const province = data.results[0].locations[0]["adminArea3"];
+            furiendFinder.city = `${city}, ${province}`;
+            $(`#location`).val(furiendFinder.city);
+            furiendFinder.getPetsNumberByAge(furiendFinder.petType, furiendFinder.city);
+        }).fail(() => {
+            furiendFinder.city = "Toronto, ON";
+            $(`#location`).val(furiendFinder.city);
+            furiendFinder.getPetsNumberByAge(furiendFinder.petType, furiendFinder.city);
+        })
+
+        
+    }
+
+    const error = (error) => {
+        console.log(`Unable to retrieve your location due to ${error.code}: ${error.message}`);
+        furiendFinder.city = "Toronto, ON";
+        $(`#location`).val(furiendFinder.city);
+        furiendFinder.getPetsNumberByAge(furiendFinder.petType, furiendFinder.city);
+    }
+
+    const geoOptions = {
+        enableHighAccuracy: true,
+        maximumAge: 30000,
+        timeout: 27000
+    };
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, error, geoOptions);
+    } else {
+        console.log("Geolocation services are not supported by your web browser.");
+        furiendFinder.city = "Toronto, ON";
+        $(`#location`).val(furiendFinder.city);
+        furiendFinder.getPetsNumberByAge(furiendFinder.petType, furiendFinder.city);
+    };
+ 
+}
+
+
 
 
 /*init method, on page load*/
 furiendFinder.init = function () {
-    const petAgeArray = ["baby", "young", "adult", "senior"];
-    /*run a for loop 4 times (0-3) and using the getPetsAvailable method to grab the pet's age and using the petFinder api query to get those words into the function*/
-    for (let i = 0; i < 4; i++) {
-        furiendFinder.getPetsAvailable(petAgeArray[i], "cat", "", "Toronto, ON", furiendFinder.getNumPets);
-    }
+    
+    // furiendFinder.getPetsNumberByAge(furiendFinder.petType,furiendFinder.city);
     $('.ageButton').on('click', furiendFinder.ageClickEvent); /*only call click event when document ready*/
     $('.adoptablePets').on('click', '.adoptionButton', furiendFinder.getMoreInfoCLickEvent);
     furiendFinder.getBreedFacts("cat");
+    furiendFinder.getGeoLocation();
     
     $('.petInformation').hide();
+    $(`#location`).val(furiendFinder.city).on("blur", furiendFinder.getLocation);
 
     
 }
 
+// Method to get all the available pets by every age
+furiendFinder.getPetsNumberByAge = function (petType,city) {
+    const petAgeArray = ["baby", "young", "adult", "senior"];
+    /*run a for loop 4 times (0-3) and using the getPetsAvailable method to grab the pet's age and using the petFinder api query to get those words into the function*/
+    for (let i = 0; i < 4; i++) {
+        furiendFinder.getPetsAvailable(petAgeArray[i], petType, city, furiendFinder.getNumPets);
+    }
+}
+
 /*find all the available pets for adoption*/
-furiendFinder.getPetsAvailable = function (petAge, petType, petBreed, city, functionCall) {
+furiendFinder.getPetsAvailable = function (petAge, petType,city, functionCall) {
     $.ajax({
         url: "https://api.petfinder.com/v2/oauth2/token",
         method: "POST",
@@ -57,14 +127,22 @@ furiendFinder.getPetsAvailable = function (petAge, petType, petBreed, city, func
                 /*50miles in km*/
                 location: city,
                 /*change later to a variable*/
-                breed: petBreed,
                 limit: 50,
                 sort: "random"
             },
         }).then((data) => {
-            functionCall(data, petAge, petType, city, petBreed);
+            furiendFinder.city = city;
+            functionCall(data, petAge, petType, city);
         })
+    }).fail((error)=>{
+        alert("You have enter an invalid location");
     });
+}
+
+furiendFinder.getLocation = function () {
+    const city = $(`#location`).val();
+    console.log(city);
+    furiendFinder.getPetsNumberByAge(furiendFinder.petType, city);
 }
 
 furiendFinder.getNumPets = function (data, petAge, petType, city, petBreed) {
@@ -90,9 +168,9 @@ furiendFinder.getBreedFacts = function (petType) {
 
 furiendFinder.ageClickEvent = function () {
     $(`.petAge`).fadeOut();
-    /*rememb er to show next page*/
+    /*remember to show next page*/
     const petAge = $(this).val();
-    furiendFinder.getPetsAvailable(petAge, "cat", "", "Toronto, ON", furiendFinder.getAdoptablePets);
+    furiendFinder.getPetsAvailable(petAge, furiendFinder.petType, furiendFinder.city, furiendFinder.getAdoptablePets);
 }
 
 furiendFinder.getMoreInfoCLickEvent = function () {
@@ -148,9 +226,11 @@ furiendFinder.adoptableButton = function (index, name, breed, url, petType, mixe
 
 /*method to display results into the html*/
 furiendFinder.appendToUl = function (totalPets, petAge) {
-    $(`.${petAge}`).append(`
-    <p>${petAge}</p>
-    <p>${totalPets}</p>`);
+
+    $(`.${petAge}`).empty().append(`
+        <p>${petAge}</p>
+        <p>${totalPets}</p>`
+    );
 }
 
 furiendFinder.appendInformation = function (name, imgUrl, gender, size, breedName, attributes, description, contact, url, mixed) {
@@ -206,7 +286,7 @@ furiendFinder.appendInformation = function (name, imgUrl, gender, size, breedNam
 
     for (attribute in attributes) {
         $(`.petFactsUl`).append(
-            `<li>${attribute.replace('_',' ')}: ${attributes[attribute]?"yes":"no"}</li>`
+            `<li>${attribute.replace('_',' ')}: ${attributes[attribute]?"✔":"✖"}</li>`
         )
     }
 
@@ -217,13 +297,16 @@ furiendFinder.appendInformation = function (name, imgUrl, gender, size, breedNam
     $(`.petLocation`).html(
         /*using conditionals for error handling API information*/
         `<p>Organization Email: ${contact.email?contact.email:"No email available"}</p>
-        <p>Organization Phone Number: ${contact.phone?contact.phone:"No phone number available"}</p>`
+        <p>Organization Phone Number: ${contact.phone ? contact.phone : "No phone number available"}</p>
+        <ul></ul>`
     )
-
+    
+    console.log(contact.address);
     for (contacts in contact.address) {
-        /*not showing null if no address available*/
-        if (contact.address[contacts] !== null) {
-            $(`.petLocation`).append(
+    /*not showing null if no address available*/
+        
+        if (contact.address[contacts] !== null && contact.address[contacts] !== "") {
+            $(`.petLocation ul`).append(
                 `<li>${contacts}: ${contact.address[contacts]}<li>`
             )
         }
@@ -237,21 +320,6 @@ furiendFinder.appendInformation = function (name, imgUrl, gender, size, breedNam
     )
 }
 
-
-// furiendFinder.createBreedsButton = function (petName, petLifeSpan, petTemperament, petOrigin, petId) {
-
-//     $('.breeds').append(
-//         `<li>
-//                 <p>Name: ${petName}</p>
-//                 <p>Origin: ${petOrigin}</p>
-//                 <p>Temperament: ${petTemperament}</p>
-//                 <p>Life Span: ${petLifeSpan}</p>
-//         </li>`
-//     )
-
-// }
-
-// furiendFinder.getPetsAvailable();
 $(document).ready(function () {
     furiendFinder.init();
     
