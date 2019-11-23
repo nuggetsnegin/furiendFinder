a429461c6efeb66ae56bef242919a5fb36951b1a
Juan Jose Acaiturri-Villa <juan.acaiturri@hotmail.com>
2019-11-23 14:39:55 -0500

    changed some positioning

--- script.js
+++ script.js
@@ -101,7 +101,7 @@ furiendFinder.init = function () {
         $(".backButton").hide();
     });
     $(`header`).on("click", `.backButtonToAge`, function () {
-        $(`.adoptionOptions`).fadeOut();
+        $(`.adoptionOptions`).hide();
         $(`.petAge`).fadeIn();
         $(".backButtonToType").show();
         $(".backButtonToAge").hide();
@@ -376,7 +376,7 @@ AOS.init({
 
 
     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-    offset: -500, // offset (in px) from the original trigger point
+    offset: 0, // offset (in px) from the original trigger point
     delay: 0, // values from 0 to 3000, with step 50ms
     duration: 400, // values from 0 to 3000, with step 50ms
     easing: 'ease', // default easing for AOS animations
